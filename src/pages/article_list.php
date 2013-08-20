2354ca3869fc1a6c599dbdf1009f063ee33ed7ff
xz <xiangzhi@lightinthebox.com>
2013-08-20 10:49:26 +0800

    article_list.php merge 后显示两个

--- src/pages/article_list.php
+++ src/pages/article_list.php
@@ -44,8 +44,8 @@
 			?>
    				 <div class="row-fluid">              
            		 	<div class="hero-unit">
-
-           		 		<h4><a class="article_title" href="?<?php echo "article_uid=".$values["article_uid"];?>"><?php 
+						<!-- href="?<?php echo "article_uid=".$values["article_uid"];?>" -->
+           		 		<h4><a class="article_title" onclick='getArticleDetail("<?php echo $values["article_uid"]; ?>")' ><?php 
            		 				echo $values["article_name"];          		 
            		 				$_SESSION["article_uid"] = 	$values["article_uid"];// session操作
            		 		?></a></h4>
@@ -53,17 +53,9 @@
               					echo substr($values["article_content"],0,140);
               					?></p>
               			<p style="font-size:50%"><?php echo  $values["article_moddate"];?></p>
-
-           		 		<h3><?php
-
-								echo $values["article_name"];
-								$_SESSION["article_uid"] = $values["article_uid"]; // session操作
-							?></h3>
-              			<p><?php
-								echo substr($values["article_content"], 0, 120);
-							?></p>
-
-              			<p><a class="btn"  onclick='getArticleDetail("<?php echo $values["article_uid"]; ?>")' >更多 &raquo;</a></p>
+						
+						<p><a class="btn" onclick='getArticleDetail("<?php echo $values["article_uid"]; ?>")' >更多 &raquo;</a></p>
+						           		 		             			
             		</div>
             	 </div>
    				<?php	
