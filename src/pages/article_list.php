a1deb4a99528f695987fddc98d074e75f7c9133f
yaoqian <yaoqian@lightinthebox.com>
2013-08-20 10:36:01 +0800

    文章列表页中文章的题目做成链接，点击可以查看文章细节。 文章列表页中添加文章的最后修改时间，并以小字体显示。 文章细节页中添加最后修改时间，并以小字体显示。 首页main.php的页首“PHP Blog”添加链接，可以链到http://phpblog.com 文章《要让对的成为赢家》的格式最为美观，建议效果展示时用此文章。

--- src/pages/article_list.php
+++ src/pages/article_list.php
@@ -11,14 +11,15 @@
    				?>
    				 <div class="row-fluid">              
            		 	<div class="hero-unit">
-           		 		<h3><?php 
+           		 		<h4><a class="article_title" href="?<?php echo "article_uid=".$values["article_uid"];?>"><?php 
            		 				echo $values["article_name"];          		 
            		 				$_SESSION["article_uid"] = 	$values["article_uid"];// session操作
-           		 		?></h3>
+           		 		?></a></h4>
               			<p><?php 
-              					echo substr($values["article_content"],0,120);
+              					echo substr($values["article_content"],0,140);
               					?></p>
-              			<p><a class="btn" href="?<?php echo "article_uid=".$values["article_uid"];?>">更多 &raquo;</a></p>
+              			<p style="font-size:50%"><?php echo  $values["article_moddate"];?></p>
+              			<p><a class="btn"  onclick='getArticleDetail("<?php echo $values["article_uid"]; ?>")' >更多 &raquo;</a></p>
             		</div>
             	 </div>
    				<?php
