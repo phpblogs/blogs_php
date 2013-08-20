9ba0a56cab6500c97f7ba1b01d995fdff22cb658
gswj0704 <gswj0704@163.com>
2013-08-20 11:20:27 +0800

    article_detail.php 页面修改

--- src/pages/article_detail.php
+++ src/pages/article_detail.php
@@ -27,8 +27,8 @@ if($more === "show")
 
 	<div class="row-fluid" id="<?php echo $aid?>">
 	   <div class="hero-unit">
-           <h3><?php echo $article_detail[0]['article_name']?></h3>
-           <p ><?php echo $article_detail[0]['article_moddate']."</br>";?>
+           <h3 ><?php echo $article_detail[0]['article_name']?></h3>
+           <p style="font-size:50%"><?php echo $article_detail[0]['article_moddate']."</br>";?>
            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $article_detail[0]['article_content']?></p>
           <p><a class="btn"  onclick='getArticleDetail("<?php echo $aid; ?>","none")' >收回 &raquo;</a></p>
           </div><!--/row--> 
@@ -43,12 +43,13 @@ else if($more ==="none")
 
 <div class="row-fluid" id="<?php echo $aid?>">
 	   <div class="hero-unit">
-           <h3><?php echo $article_detail[0]['article_name']?></h3>
-           <p ><?php echo $article_detail[0]['article_moddate']."</br>";?>
-           <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo substr($article_detail[0]['article_content'], 0, 120)?></p>
-          <p><a class="btn"  onclick='getArticleDetail("<?php echo $aid; ?>","show")' >更多 &raquo;</a></p>
+           <h4><?php echo $article_detail[0]['article_name']?></h4>
+           <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo substr($article_detail[0]['article_content'], 0, 140)?></p>
+		   <p style="font-size:50%"><?php echo  $values["article_moddate"];?></p>
+          <p><a class="btn"  onclick='getArticleDetail("<?php echo $aid; ?>","show")' >展开 &raquo;</a></p>
           </div><!--/row--> 
        </div>
+	   
        
  <?php 
 }
